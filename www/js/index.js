4b76d9def892617675a337740e8507f8df91cc33
fabio franci <fabio.franci@gmail.com>
2016-07-15 10:46:01 +0200

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -1165,10 +1165,24 @@ function onDeviceReady() {
     });
 
     function aggiungiClienteDiOggi(nuovocliente) {
-
-        alert("Eccomi qui");
-        alert(nuovocliente.id_sede_cliente);
-
+        clienti_di_oggi[]=nuovocliente.id_sede_cliente;
+        $("#totclientidioggi").html(clienti_di_oggi.length);
+        $.each(sedi_clienti_server, function (index, cliente) {
+            if (cliente.id==nuovocliente.id_sede_cliente) {
+                rigaselect="INSERT OR REPLACE INTO SERVER_SEDI_CLIENTI (id, cliente_e_sede, sede, indirizzo, CAP, citta, provincia, persona_di_riferimento, telefono, email, note, ultimo_aggiornamento) SELECT '"+cliente.id+"' AS id, '"+cliente.cliente_e_sede+"' AS cliente_e_sede, '"+cliente.sede+"' as sede, '"+cliente.indirizzo+"' AS indirizzo,'"+cliente.CAP+"' AS CAP, '"+cliente.citta+"' AS citta,'"+cliente.provincia+"' AS provincia, '"+cliente.persona_di_riferimento+"' AS persona_di_riferimento,'"+cliente.telefono+"' AS telefono, '"+cliente.email+"' AS email,'"+cliente.note+"' AS note,'"+local_ultimo_aggiornamento+"' AS ultimo_aggiornamento  ";
+            }
+        });
+        console.log(rigaselect);
+        if (rigaselect) {
+            //ora può lanciare la transazione
+            db.transaction(
+                function (tx3) { tx3.executeSql(rigaselect); },
+                onDbError,
+                function() {
+                    alert("Cliente di oggi aggiunto");
+                }
+            );
+        }
     }
 
     function aggiungiPostazione(nuovapostazione) {
